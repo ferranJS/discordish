f8c04f034901c35bca982ae7dc12aca652697509
Ferran <ferran@berebel.io>
2023-03-21 02:22:00 +0100

    side-bar done! (partially but enough 😅)

--- src/app/layout/side-bar/side-bar.component.ts
+++ src/app/layout/side-bar/side-bar.component.ts
@@ -10,9 +10,25 @@ import { Server } from "src/app/shared/interfaces"
       flex flex-col fixed bg-zinc-800 h-screen
       gap-2 p-3 pb-2 pt-2 m-0 top-0 left-0
     ">
-      <side-bar-button *ngFor="let server of servers" [server]="server"/>
-      <side-bar-button [icon]="'plus'"/>
-      <side-bar-button [icon]="'compass'"/>
+      <side-bar-button
+        [iconClass]="'fa-brands fa-discord'"
+        [textName]="'Mensajes directos'" 
+        [color]="'blue'"
+        [selectable]="true"/>
+      <hr class="w-8 self-center border-zinc-700 border-[1.5px]">
+      <side-bar-button
+        *ngFor="let server of servers"
+        [color]="'blue'"
+        [server]="server"/>
+      <side-bar-button
+        [iconClass]="'fa-solid fa-plus'"
+        [color]="'green'"
+        [textName]="'Añadir un servidor'"/>
+      <side-bar-button
+        [iconClass]="'fa-solid fa-compass'"
+        [textName]="'Explorar servidores públicos'"
+        [color]="'green'"
+        [selectable]="true"/>
     </div>
     `,
     imports: [CommonModule, SideBarButtonComponent]
@@ -25,6 +41,10 @@ export class SideBarComponent {
     },
     {
       name: 'El servidor más Wapo!',
+      image: ''
+    },
+    {
+      name: 'El servidor de ferrran junio asdfasdf asdf asdf asdfasd fasdf asdf',
       image: ''
     }
   ]
