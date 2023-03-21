d640716deb082fdfc5ef4e1ec2524da10e21a042
Ferran <ferran@berebel.io>
2023-03-22 00:35:21 +0100

    configs & line-clamp tw plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   mode: 'jit',
+   content: [
+      "./src/**/*.{html,ts}",
+   ],
+   theme: {
+      extend: {},
+   },
+   plugins: [
+      require('@tailwindcss/line-clamp')
+   ],
 }
