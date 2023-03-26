4277de2126b04ed8ce3678222dc616cb0947db8d
Ferran <ferran@berebel.io>
2023-03-26 20:16:45 +0200

    styles, main container and login

---
+++ src/app/layout/main-container/main-container.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+   selector: 'main-container',
+   standalone: true,
+   imports: [CommonModule, RouterModule],
+   template: `
+   `,
+   styles: [':host { @apply w-full h-full }'],
+   changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainContainerComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ const appRoutes: Route[] = [
     loadComponent: () => import("./app/layout/layout.component").then((m) => m.LayoutComponent),
     loadChildren: () => import("./app/layout/layout.routes").then((m) => m.routes),
   },
+  {
+   path: "login",
+   loadComponent: () => import("./app/login/login.component").then((m) => m.LoginComponent),
+  },
 ];
 
 bootstrapApplication(AppComponent, { providers: [provideRouter(appRoutes)] }).catch((err) =>
